20a34dd303286d6058c72cf1b2b833a0031b145b
Sohee <sohee3480@gmail.com>
2021-06-05 15:16:19 +0900

    상세 화면 실제 데이터 반영

--- app/src/main/java/com/example/pizzaorderapp_sohee/ViewStoreDetailActivity.kt
+++ app/src/main/java/com/example/pizzaorderapp_sohee/ViewStoreDetailActivity.kt
@@ -2,8 +2,14 @@ package com.example.pizzaorderapp_sohee
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.bumptech.glide.Glide
+import com.example.pizzaorderapp_sohee.datas.Store
+import kotlinx.android.synthetic.main.activity_view_store_detail.*
 
 class ViewStoreDetailActivity : BaseActicity() {
+
+    lateinit var mStoreData : Store
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -16,6 +22,15 @@ class ViewStoreDetailActivity : BaseActicity() {
     }
 
     override fun setValues() {
+
+        mStoreData = intent.getSerializableExtra("storeData") as Store
+
+        Glide.with(mContext).load(mStoreData.logoURL).into(storeLogoImg)
+
+        storePhoneNumTxt.text = mStoreData.phoneNum
+
+        storeNameTxt.text = mStoreData.name
+
     }
 
 }
