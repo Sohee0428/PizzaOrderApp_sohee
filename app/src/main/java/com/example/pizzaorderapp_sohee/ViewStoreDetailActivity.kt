422b87d411cb09c82fe95c2bdc33b90cbcc70c89
Sohee <sohee3480@gmail.com>
2021-06-05 14:55:14 +0900

    가게 상세보기 화면 추가

---
+++ app/src/main/java/com/example/pizzaorderapp_sohee/ViewStoreDetailActivity.kt
@@ -0,0 +1,21 @@
+package com.example.pizzaorderapp_sohee
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class ViewStoreDetailActivity : BaseActicity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_view_store_detail)
+        setupEvents()
+        setValues()
+    }
+
+    override fun setupEvents() {
+    }
+
+    override fun setValues() {
+    }
+
+}
