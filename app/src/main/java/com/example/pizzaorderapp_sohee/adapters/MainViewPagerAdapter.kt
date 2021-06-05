0549ecb184a50e1d0c5f9c78d5fc75a6a220ee5c
Sohee <sohee3480@gmail.com>
2021-06-05 14:04:55 +0900

    페이지 제목 설정+ 글씨 색 변경

--- app/src/main/java/com/example/pizzaorderapp_sohee/adapters/MainViewPagerAdapter.kt
+++ app/src/main/java/com/example/pizzaorderapp_sohee/adapters/MainViewPagerAdapter.kt
@@ -7,6 +7,14 @@ import com.example.pizzaorderapp_sohee.fragments.ChickenStoreListFragment
 import com.example.pizzaorderapp_sohee.fragments.PizzaStoreListFragment
 
 class MainViewPagerAdapter(fm : FragmentManager) :FragmentPagerAdapter(fm){
+
+    override fun getPageTitle(position: Int): CharSequence? {
+        return when(position){
+            0 -> "피자 가게"
+            else -> "치킨 가게"
+        }
+    }
+
     override fun getItem(position: Int): Fragment {
        return when(position) {
            0-> PizzaStoreListFragment()
