dc82a81c587100d4a708f23c80f5ed7df4380ab6
Sohee <sohee3480@gmail.com>
2021-06-05 13:47:44 +0900

    치킨 가게 목록 표시용 프래그먼트 기초 구조 추가

---
+++ app/src/main/java/com/example/pizzaorderapp_sohee/fragments/ChickenStoreListFragment.kt
@@ -0,0 +1,21 @@
+package com.example.pizzaorderapp_sohee.fragments
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.example.pizzaorderapp_sohee.R
+
+class ChickenStoreListFragment : Fragment() {
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+        return inflater.inflate(R.layout.fragment_list_store_chicken, container, false)
+    }
+
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        super.onActivityCreated(savedInstanceState)
+
+//        동작 관련 코드
+    }
+}
