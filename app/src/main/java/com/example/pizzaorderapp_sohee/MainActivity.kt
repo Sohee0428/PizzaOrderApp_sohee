8070ac81ff22db0188cc349a5d9a0fed60bb0dae
Sohee <sohee3480@gmail.com>
2021-06-05 14:01:54 +0900

    뷰페이지 + 탭레이아웃 연결까지

--- app/src/main/java/com/example/pizzaorderapp_sohee/MainActivity.kt
+++ app/src/main/java/com/example/pizzaorderapp_sohee/MainActivity.kt
@@ -2,9 +2,13 @@ package com.example.pizzaorderapp_sohee
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.pizzaorderapp_sohee.adapters.MainViewPagerAdapter
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : BaseActicity() {
+
+    lateinit var mvpa : MainViewPagerAdapter
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
@@ -17,18 +21,16 @@ class MainActivity : BaseActicity() {
     }
 
     override fun setValues() {
-//        화면에 데이터르 ㄹ표시하기 위한 코드 모음
+//        화면에 데이터를 표시하기 위한 코드 모음
 
 //        뷰 페이저 작업
 
-
+        mvpa = MainViewPagerAdapter(supportFragmentManager)
+        mainViewPager.adapter = mvpa
 
 //        탭레이아웃과 연결
 
-
-
-
-//        mainTabLayout.setupWithViewPager()
+        mainTabLayout.setupWithViewPager(mainViewPager)
 
 
     }
